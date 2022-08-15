12ac596292b18a9926f96091d5c200975918dbf2
Max Max <mm3681525@gmail.com>
2022-08-15 14:30:01 +0000

    Added sonarqube plugins

--- project.clj
+++ project.clj
@@ -4,16 +4,23 @@
   :license {:name "MIT"
     :url "https://github.com/kenreilly/clojure-example/blob/master/LICENSE"}
   :dependencies [
-    [org.clojure/clojure "1.10.0"]
-    [org.clojure/data.json "0.2.6"]
-    [org.clojure/java.jdbc "0.7.10"]
-    [org.postgresql/postgresql "42.1.4"]
-    [ring/ring-defaults "0.3.2"]
-    [ring/ring-devel "1.6.3"]
-    [ring/ring-json "0.5.0"]
-    [compojure "1.6.1"]
-    [http-kit "2.3.0"]
-    [lynxeyes/dotenv "1.0.2"]]
+    [org.clojure/clojure "1.11.1"]
+    [org.clojure/data.json "2.4.0"]
+    [org.clojure/java.jdbc "0.7.12"]
+    [org.postgresql/postgresql "42.4.1"]
+    [ring/ring-defaults "0.3.3"]
+    [ring/ring-devel "1.9.5"]
+    [ring/ring-json "0.5.1"]
+    [compojure "1.7.0"]
+    [http-kit "2.6.0"]
+    [lynxeyes/dotenv "1.1.0"]]
+  :plugins [[lein-ancient "0.6.15"]
+          [lein-test-report "0.2.0"]
+          [lein-test-report-junit-xml "0.2.0"]
+          [jonase/eastwood "1.2.4"]
+          [lein-cloverage "1.0.13"]
+          [lein-kibit "0.1.6"]
+          [lein-nvd "0.6.0"]]
   :main ^:skip-aot clojure-example.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all} :dev {:main clojure-example.core/-dev-main}})
